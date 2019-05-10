ea3d1249b94731116b67b045b8045d046ba7bac5
LeeReindeer <reindeerlee.work@gmail.com>
2019-05-10 20:59:25 +0800

    :sparkles: Add timer functions

--- ros_timer.c
+++ ros_timer.c
@@ -1,2 +1,102 @@
 #include "ros_timer.h"
-#include "ros.h"
+#include <stdio.h>
+
+static ROS_TIMER *timer_queue;
+static uint32_t ros_sys_ticks = 0;
+static char day_time[9];  // format: 00:00:00
+
+static void wakeup_task(ROS_TCB *tcb) {
+  CRITICAL_STORE;
+  CRITICAL_START();
+  tcb->status = TASK_READY;
+  ros_tcb_enqueue(tcb);
+  CRITICAL_END();
+}
+
+// dec the ticks of every timer in timer_queue, wake up task if ticks == 0
+void ros_check_timer() {
+  // TODO
+}
+
+// delay current tcb
+status_t ros_delay(uint32_t ticks) {
+  ROS_TIMER timer;
+  ROS_TCB *cur_tcb;
+  uint8_t status;
+  CRITICAL_STORE;
+  cur_tcb = ros_current_tcb();
+  if (ticks == 0) {
+    status = ROS_ERR_PARAM;
+  } else if (cur_tcb == NULL) {
+    status = ROS_ERR_CONTEXT;
+  }
+  eles {
+    CRITICAL_START();
+    cur_tcb->status = TASK_BLOCKED;
+    timer.ticks = ticks;
+    timer.blocked_tcb = cur_tcb;
+    if (ros_register_timer(&timer) != ROS_OK) {
+      status = ROS_ERR_TIMER;
+      CRITICAL_END();
+    } else {
+      status = ROS_OK;
+      CRITICAL_END();
+      // call scheduler to swap out current task
+      ros_schedule();
+    }
+  }
+  return status;
+}
+
+// insert timer to the timer queue head
+status_t ros_register_timer(ROS_TIMER *timer) {
+  if (timer == NULL || timer->blocked_tcb == NULL) {
+    return ROS_ERR_PARAM;
+  }
+  CRITICAL_STORE;
+  CRITICAL_START();
+  if (timer_queue == NULL) {
+    timer_queue = timer;
+    timer_queue->next_timer = NULL;
+  } else {
+    timer->next_timer = timer_queue;
+    timer_queue = timer;
+  }
+  CRITICAL_END();
+  return ROS_OK;
+}
+
+void ros_sys_tick() {
+  if (ROS_STARTED) {
+    ros_sys_ticks++;
+    // check for any delay task is due
+    ros_check_timer();
+  } else {
+    // #warning "ROS not started, please call ros_init() first."
+  }
+}
+
+void ros_set_sys_tick(uint32_t ticks) { ros_sys_ticks = ticks; }
+
+uint32_t ros_get_sys_tick() { return ros_sys_ticks; }
+
+status_t ros_set_time(uint8_t hour, uint8_t minute, uint8_t second) {
+  if (hour < 0 || hour > 23 || minute < 0 || minute > 59 || second < 0 ||
+      second > 59) {
+    return ROS_ERR_PARAM;
+  }
+  ros_sys_ticks = (hour * 3600U + minute * 60U + second) * 100U;
+  return ROS_OK;
+}
+
+char *ros_get_time() {
+  uint32_t cur_second = ros_sys_ticks % 8640000U / 100U;
+  uint8_t hour = 0, minute = 0, second = 0;
+  hour = cur_second / 3600U;
+  cur_second %= 3600U;
+  minute = cur_second / 60U;
+  cur_second %= 60U;
+  second = cur_second;
+  sprintf(day_time, "%02d:%02d:%02d", hour, minute, second);
+  return day_time;
+}
--- ros_timer.h
+++ ros_timer.h
@@ -1,4 +1,30 @@
-#ifndef __ROS_H__
-#define __ROS_H__
+#ifndef __ROS_TIMER_H__
+#define __ROS_TIMER_H__
+
+#include "ros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ros_timer {
+  ROS_TCB *blocked_tcb;
+  uint32_t ticks;
+  struct ros_timer *next_timer;
+} ROS_TIMER;
+
+void ros_check_timer();
+// add the timer to timer queue
+status_t ros_register_timer(ROS_TIMER *timer);
+status_t ros_delay(uint32_t ticks);
+void ros_set_sys_tick(uint32_t ticks);
+uint32_t ros_get_sys_tick();
+// transfer system tick to the time in the real world in 24-hours format
+void ros_set_time(uint8_t hour, uint8_t minute, uint8_t second);
+char* ros_get_time();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  //__ROS_H__
