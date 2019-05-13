ec6bfdab79e5178a0a77c9c29606c5abd8cf00ed
LeeReindeer <reindeerlee.work@gmail.com>
2019-05-13 13:10:21 +0800

    :ok_hand: Add main example

---
+++ main.c
@@ -0,0 +1,63 @@
+/**
+ * Blink example in ROS, DO NOT use any function in Arduino.h
+ */
+#include <avr/io.h>
+#include "ros.h"
+
+ROS_TCB task1;
+ROS_TCB task2;
+uint8_t task1_stack[ROS_DEFAULT_STACK_SIZE];
+uint8_t task2_stack[ROS_DEFAULT_STACK_SIZE];
+
+#define LED1 13
+#define LED2 12
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+
+#define TASK1_PRIORITY 1
+#define TASK2_PRIORITY 0  // max priority
+
+void t1() {
+  while (1) {
+    // set LED1 high
+    bitSet(PORTB, 5);
+    ros_delay(200);
+    bitClear(PORTB, 5);
+    ros_delay(200);
+  }
+}
+
+void t2() {
+  while (1) {
+    bitSet(PORTB, 4);
+    // delay a second
+    ros_delay(100);
+    bitClear(PORTB, 4);
+    ros_delay(100);
+  }
+}
+
+void setup() {
+  // set LED13 and LED 12 as output
+  bitSet(DDRB, 5);
+  bitSet(DDRB, 4);
+  bool os_started = ros_init();
+  if (os_started) {
+    ros_create_task(&task1, t1, TASK1_PRIORITY,
+                    STACK_POINT(task1_stack, ROS_DEFAULT_STACK_SIZE));
+    ros_create_task(&task2, t2, TASK2_PRIORITY,
+                    STACK_POINT(task2_stack, ROS_DEFAULT_STACK_SIZE));
+    ros_schedule();
+  }
+}
+
+void loop() {
+  // nothing
+}
+
+int main() {
+  setup();
+  // never call loop
+  loop();
+  return 0;
+}
